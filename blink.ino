acfdcc499894c3a2beac31ba7f8467b193192d88
phanudet2016 <5706021622141@fitm.kmutnb.ac.th>
2017-10-02 10:31:56 +0700

    Lab#1 Blink
    Lab#1 Blink

---
+++ blink.ino
@@ -0,0 +1,16 @@
+const int Node_LED = 5; //กำหนดให้ Node_LED แทนชื่อ digital I/O ขาที่ 2 ของ NodeMCU (Built in LED)
+void setup() 
+{
+  // put your setup code here, to run once:
+  pinMode(Node_LED,OUTPUT); //กำหนดให้ Node_LED มีรูปแบบการทำงานในโหมดเอาต์พุต
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+  digitalWrite(Node_LED,HIGH);  //กำหนดให้ส่งสัญญาณเอาต์พุต HIGH (3.3V) ออกทางขา Node_LED 
+  delay(200); //หน่วงเวลา 200 มิลลิวินาที
+  digitalWrite(Node_LED,LOW); //กำหนดให้ส่งสัญญาณเอาต์พุต LOW (0V) ออกทางขา Node_LED 
+  delay(200); //หน่วงเวลา 200 มิลลิวินาที
+
+}
